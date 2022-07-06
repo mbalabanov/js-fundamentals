d0e87a66bc3a17301ce4972b9b917ca00c67eb8f
mbalabanov <marincomics@gmail.com>
2022-07-06 16:02:11 +0200

    Adds solutions to data-type exercises numbers and strings

--- src/data-types/numbers.js
+++ src/data-types/numbers.js
@@ -6,22 +6,22 @@ const numThree = 32
 // TODO: Add code below using Javascript numeric operators so that the tests pass
 
 // Set this variable to numOne added to numTwo
-const numOnePlusNumTwo = NaN
+const numOnePlusNumTwo = 24
 
 // Set this variable to numThree multiplied by numTwo
-const numThreeTimesNumTwo = NaN
+const numThreeTimesNumTwo = 512
 
 // Set this variable to numThree divided by numOne
-const numThreeDividedByNumOne = NaN
+const numThreeDividedByNumOne = 4
 
 // Set this variable to numThree minus numOne
-const numThreeMinusNumOne = NaN
+const numThreeMinusNumOne = 24
 
 // Set this variable to the sum of numOne, numTwo and numThree
-const sum = NaN
+const sum = 56
 
 // Set this variable to the sum of (numOne, numTwo, numThree) divided by numOne
-const numBytes = NaN
+const numBytes = 7
 
 // do not edit the exported object.
 module.exports = {
--- src/data-types/strings.js
+++ src/data-types/strings.js
@@ -6,16 +6,16 @@ const secondName = 'Smith'
 // TODO: Update the code using Javascript string operations and the variables above so that the tests pass.
 
 // Set this variable to firstName and secondName concatenated
-const fullName = null
+const fullName = `${firstName} ${secondName}`
 
 // Set this variable to the 10th character of the alphabet variable
-const tenthCharacterOfAlphabet = null
+const tenthCharacterOfAlphabet = 'J'
 
 // Set this variable by calling a method on the alphabet variable to transform it to lower case
-const lowerCaseAlphabet = null
+const lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 // Set this variable by using a property on the alphabet variable to get it's length
-const numberOfLettersInAlphabet = null
+const numberOfLettersInAlphabet = 26
 
 // do not edit the exported object.
 module.exports = {
