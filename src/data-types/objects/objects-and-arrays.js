9dc2037cc6005bd04c2481be9030655d130824e1
mbalabanov <marincomics@gmail.com>
2022-07-07 16:56:22 +0200

    Solves third objects exercise - objects-and-arrays.js

--- src/data-types/objects/objects-and-arrays.js
+++ src/data-types/objects/objects-and-arrays.js
@@ -12,20 +12,20 @@ const basket = {
       price: 0.5
     }
   ],
-  voucherCodes: [
-    'AA-AA-A',
-    'BB-BB-B'
-  ]
+  voucherCodes: ['AA-AA-A', 'BB-BB-B']
 }
 
 // TODO: write code in this section to pass the tests. You will need to add new code
 // as well as modify some of the existing code
 
 // Set this variable to the length of the baskets voucher codes array
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // Set this variable to the first element in of the baskets voucher codes array
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
+
+basket.items[0].price = 2
+basket.items.push({ name: 'Oranges', quantity: 4, price: 0.75 })
 
 // Do not edit this exported object
 module.exports = {
