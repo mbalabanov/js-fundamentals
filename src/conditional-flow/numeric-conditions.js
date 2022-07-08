f0cc28bfad10aeaba48e556104277b97221c92ca
Marin Balabanov <marincomics@gmail.com>
2022-07-08 17:14:31 +0200

    Solves conditional-flow/numeric-conditions.js

--- src/conditional-flow/numeric-conditions.js
+++ src/conditional-flow/numeric-conditions.js
@@ -7,10 +7,21 @@ const ARRAY_TWO = ['Hello', 'Conditions']
 // or true if it is empty
 let answerOne
 
+if (ARRAY_ONE.length !== 0) {
+    answerOne = false
+} else {
+    answerOne = true
+}
+
 // Use conditional statements to set answerTwo to false if ARRAY_TWO is not empty
 // or true if it is empty
 let answerTwo
 
+if (ARRAY_TWO.length > 0) {
+    answerTwo = false
+} else {
+    answerTwo = true
+}
 
 
 const NUM_ONE = 92837
@@ -19,15 +30,23 @@ const NUM_TWO = 92827
 // Use conditional statements to set answerThree to true if NUM_ONE is more than NUM_TWO
 let answerThree
 
-
+if (NUM_ONE > NUM_TWO) {
+    answerThree = true
+}
 
 const NUMBERS = [67, 2039, 17, 501, 98, 139, 21]
 
 // Use a combination of a loop and conditional statements to set answerFour
 // to the lowest number in the NUMBERS array
 let answerFour
+let comparisonNumber = NUMBERS[0]
 
-
+for (let currentIndex = 0; currentIndex <= NUMBERS.length; currentIndex++) {
+    if (NUMBERS[currentIndex] < comparisonNumber) {
+        comparisonNumber = NUMBERS[currentIndex]
+    }
+}
+answerFour = comparisonNumber
 
 
 // Don't edit the code below this line
