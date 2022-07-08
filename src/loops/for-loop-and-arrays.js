1e1b0467e9370b2031aac3128ac5034a58cc1df5
Marin Balabanov <marincomics@gmail.com>
2022-07-08 18:43:17 +0200

    Solves all loops exercises

--- src/loops/for-loop-and-arrays.js
+++ src/loops/for-loop-and-arrays.js
@@ -6,19 +6,41 @@ let word = ''
 // TODO: Add code below this line to make the tests pass
 
 // Use a for loop to set the sum variable to the sum of all the values in nums
-sum = 0
+nums.forEach(currentNum => {
+  sum = sum + currentNum
+});
+
+
 
 // Use a for loop to populate doubledNums with every value from nums array doubled (i.e [2, 6, 24, etc...])
 const doubledNums = []
 
+nums.forEach(currentNum2 => {
+  doubledNums.push( currentNum2 * 2 )
+});
+
 // Use a for loop to set word equal to all the letters in the letters array
 word = ''
+
+letters.forEach(currentLetter => {
+  word += currentLetter
+});
 
 // Use a for loop to populate everySecondNum with every second number from the nums array
 const everySecondNum = []
 
+for (let currentNumber = 0; currentNumber < nums.length; currentNumber++) {
+  if (currentNumber % 2 !== 0) {
+    everySecondNum.push(nums[currentNumber])
+  }
+}
+
 // Use a for loop to populate numsReversed with the numbers from nums in reverse order
 const numsReversed = []
+
+for (let currentReversedNumber = nums.length - 1; currentReversedNumber >= 0; currentReversedNumber--) {
+  numsReversed.push(nums[currentReversedNumber])
+}
 
 // do not change below this line
 module.exports = {
--- src/loops/for-loop-basic.js
+++ src/loops/for-loop-basic.js
@@ -5,11 +5,29 @@ const countdown = []
 
 // TODO: Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
 
+for (let currentNumber = 0; currentNumber <= 3; currentNumber++) {
+  numsZeroToThree.push(currentNumber)
+}
+
 // TODO: Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
+
+for (let currentNumber = 5; currentNumber <= 10; currentNumber++) {
+  numsFiveToTen.push(currentNumber)
+}
 
 // TODO: Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
 
+for (let currentNumber = 0; currentNumber <= 6; currentNumber++) {
+  if (currentNumber % 2 === 0) {
+    evenNums.push(currentNumber)
+  }
+}
+
 // TODO: Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
+
+for (let currentNumber = 3; currentNumber >= 0; currentNumber--) {
+  countdown.push(currentNumber)
+}
 
 // do not change below this line
 module.exports = {
