2d0f79249a47634b29dfc606337bb7a2f3f2ee06
Marin Balabanov <marincomics@gmail.com>
2022-07-08 16:39:21 +0200

    Solves conditional-flow/boolean-conditions.js

--- src/conditional-flow/boolean-conditions.js
+++ src/conditional-flow/boolean-conditions.js
@@ -1,12 +1,16 @@
 // Initialise the didPass variable with a boolean value
-let didPass
+let didPass = true
 
 // Create a conditional statement that changes the answer variable to the string
 // "Well done, you passed!" if didPass is true, or "Sorry, try again!" if didPass
 // is false
 let answer
 
-
+if (didPass) {
+  answer = "Well done, you passed!"
+} else {
+  answer = "Sorry, try again!"
+}
 
 
 // When you're done and the test passes, changing didPass to the opposite boolean
